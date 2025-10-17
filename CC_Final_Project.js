d973483135d7f5a9ba2cb7585830bb7d4c6146b5
CoffeeCakeSnake <a.facsko@icloud.com>
2025-10-17 11:34:48 +0200

    Adding randomness to Earth
    The trees now spawn randomly and have some kind of uniqueness to them, making them look more organic.

--- CC_Final_Project.js
+++ CC_Final_Project.js
@@ -1,44 +1,81 @@
-let showEarth = true;
-let showFire = false;
-let showWater = false;
-let showAir = false;
-
 function setup() {
     createCanvas(innerWidth, innerHeight);
     background(0);
+    frameRate(5);
+}
+
+function draw() {
+    background(0, 40);
+
+    if (growth < maxDepth) {
+        growth += growSpeed;
+    }
+
     earth();
 }
 
-// earth
+// ------------------ EARTH ------------------ //
 // recursive tree code by Daniel Shiffman
-function branch(length) {
+// https://natureofcode.com/fractals/
+
+let showEarth = true;
+let trees = [];
+let spawnTime = 0;
+let maxTrees = 5;
+let maxDepth = 15;
+let growSpeed = 0.3;
+let growth = 0;
+
+function branch(length, depth) {
+    if (depth > growth) return;
+
+    let shade = map(depth, 0, maxDepth, 80, 200);
+    let branchStroke = map(length, 120, 2, 8, 0.5);
+    strokeWeight(branchStroke);
+    
+    stroke(shade + 70, shade + 10, 0);
     line(0, 0, 0, -length);
     translate(0, -length);
     
     if(length > 2) {
+        let shrink = random(0.55, 0.85);
+        let angle = random(PI / 8, PI / 5);
+
         push();
-        rotate(PI / 6);
-        branch(length / 3);
+        rotate(angle);
+        branch(length * shrink, depth + 1);
         pop();
 
         push();
-        rotate(- PI / 6); 
-        branch(length / 2);
+        rotate(-angle); 
+        branch(length * shrink, depth + 1);
         pop();
     }
 }
 
 function earth() {
-    stroke(150, 100, 0);
     translate(random(width), height);
-    branch(120);
+    branch(120, 0);
 }
+
+// ------------------ FIRE ------------------ //
+
+let showFire = false;
 
 function fire() {
 }
 
+// ------------------ WATER ------------------ //
+
+let showWater = false;
+
 function water() {
+
 }
+
+// ------------------ AIR ------------------ //
+
+let showAir = false;
 
 function air() {
 }
